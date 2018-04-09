6d96b82eedd3ef0b48308322cc017b353d236440
David Schwartz <dbschwartz@protonmail.com>
2018-04-09 11:13:24 -0700

    initial token list and assets

---
+++ README.md
@@ -0,0 +1,5 @@
+## NEP5 Token List
+
+A list of NEP5 tokens on MainNet. This is useful for NEO wallets that would like to get all the scripthashes and relevant data for each token.
+
+We are looking for high svg (ideally) or png files for each NEP5 token. Please upload assets to this repo under either assets/svg or assets/png, run `yarn` and then `yarn start` and submit a PR with the updated tokenList.json file.
---
+++ index.js
@@ -0,0 +1,50 @@
+const fetch = require('isomorphic-fetch')
+const fs = require('fs')
+const api = require('@cityofzion/neon-js').api
+
+const tokenDataUrl = 'http://notifications.neeeo.org/v1/tokens'
+const net = 'MainNet'
+const NETWORK_ID = {
+  MAINNET: '1'
+}
+let promises
+const tokenData = {}
+
+api.loadBalance(api.getRPCEndpointFrom, { net }).then(endpoint => {
+  fetch(tokenDataUrl)
+    .then(data => data.json())
+    .then(response => {
+      promises = response.results.map(({ token }) => {
+        const { decimals, symbol, script_hash, name } = token
+        const scriptHash = script_hash.startsWith('0x')
+          ? script_hash.slice(2)
+          : script_hash
+        return api.nep5.getToken(endpoint, scriptHash).then(tokenInfo => {
+          let image = ''
+          const formattedSymbol = symbol.toLowerCase()
+          if (fs.existsSync(`./assets/svg/${formattedSymbol}.svg`)) {
+            image = `./assets/svg/${formattedSymbol}.svg`
+          } else if (fs.existsSync(`./assets/png/${formattedSymbol}.png`)) {
+            image = `./assets/png/${formattedSymbol}.png`
+          }
+          tokenData[symbol] = {
+            symbol,
+            companyName: name,
+            type: 'NEP5',
+            networks: {
+              [NETWORK_ID.MAINNET]: {
+                name,
+                hash: scriptHash,
+                decimals,
+                totalSupply: tokenInfo && tokenInfo.totalSupply
+              }
+            },
+            image
+          }
+        })
+      })
+      Promise.all(promises).then(() => {
+        fs.writeFile('tokenList.json', JSON.stringify(tokenData))
+      })
+    })
+})
