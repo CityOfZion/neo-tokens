5674e662dc011ea0bb446f0ba4f0100a765cb64a
David Schwartz <dbschwartz@protonmail.com>
2018-04-10 13:42:06 -0700

    add linting, update readme, and test circleci working with ontology

--- index.js
+++ index.js
@@ -5,46 +5,52 @@ const api = require('@cityofzion/neon-js').api
 const tokenDataUrl = 'http://notifications.neeeo.org/v1/tokens'
 const net = 'MainNet'
 const NETWORK_ID = {
-  MAINNET: '1'
+	MAINNET: '1'
 }
 let promises
 const tokenData = {}
 
+const blackList = {
+	NRV: '2e25d2127e0240c6deaf35394702feb236d4d7fc'
+}
+
 api.loadBalance(api.getRPCEndpointFrom, { net }).then(endpoint => {
-  fetch(tokenDataUrl)
-    .then(data => data.json())
-    .then(response => {
-      promises = response.results.map(({ token }) => {
-        const { decimals, symbol, script_hash, name } = token
-        const scriptHash = script_hash.startsWith('0x')
-          ? script_hash.slice(2)
-          : script_hash
-        return api.nep5.getToken(endpoint, scriptHash).then(tokenInfo => {
-          let image = ''
-          const formattedSymbol = symbol.toLowerCase()
-          if (fs.existsSync(`./assets/svg/${formattedSymbol}.svg`)) {
-            image = `./assets/svg/${formattedSymbol}.svg`
-          } else if (fs.existsSync(`./assets/png/${formattedSymbol}.png`)) {
-            image = `./assets/png/${formattedSymbol}.png`
-          }
-          tokenData[symbol] = {
-            symbol,
-            companyName: name,
-            type: 'NEP5',
-            networks: {
-              [NETWORK_ID.MAINNET]: {
-                name,
-                hash: scriptHash,
-                decimals,
-                totalSupply: tokenInfo && tokenInfo.totalSupply
-              }
-            },
-            image
-          }
-        })
-      })
-      Promise.all(promises).then(() => {
-        fs.writeFile('tokenList.json', JSON.stringify(tokenData, null, 2))
-      })
-    })
+	fetch(tokenDataUrl)
+		.then(data => data.json())
+		.then(response => {
+			promises = response.results.map(({ token }) => {
+				const { decimals, symbol, script_hash, name } = token
+				const scriptHash = script_hash.startsWith('0x')
+					? script_hash.slice(2)
+					: script_hash
+				return api.nep5.getToken(endpoint, scriptHash).then(tokenInfo => {
+					let image = ''
+					const formattedSymbol = symbol.toLowerCase()
+					if (fs.existsSync(`./assets/svg/${formattedSymbol}.svg`)) {
+						image = `./assets/svg/${formattedSymbol}.svg`
+					} else if (fs.existsSync(`./assets/png/${formattedSymbol}.png`)) {
+						image = `./assets/png/${formattedSymbol}.png`
+					}
+					if (!blackList[symbol] && blackList[symbol] !== scriptHash) {
+						tokenData[symbol] = {
+							symbol,
+							companyName: name,
+							type: 'NEP5',
+							networks: {
+								[NETWORK_ID.MAINNET]: {
+									name,
+									hash: scriptHash,
+									decimals,
+									totalSupply: tokenInfo && tokenInfo.totalSupply
+								}
+							},
+							image
+						}
+					}
+				})
+			})
+			Promise.all(promises).then(() => {
+				fs.writeFile('tokenList.json', JSON.stringify(tokenData, null, 2))
+			})
+		})
 })
