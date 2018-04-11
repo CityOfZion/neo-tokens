caa2848676277e5040656c2c9bc7c6af43fb657e
David Schwartz <dbschwartz@protonmail.com>
2018-04-11 09:33:20 -0700

    initial commit neo-tokens branch

---
+++ README.md
@@ -0,0 +1,7 @@
+## NEP5 Token List
+
+A list of NEP5 tokens on MainNet. This is useful for NEO wallets that would like to get all the scripthashes and relevant data for each token.
+
+Step 1: wait until the NEP5 token is listed here (http://notifications.neeeo.org/v1/tokens)
+Step 2: upload NEP5 token image to assets/svg or assets/png
+Step 3: submit PR (tokenList.json will automatically be updated through CircleCI)
---
+++ index.js
@@ -0,0 +1,59 @@
+const fetch = require('isomorphic-fetch')
+const fs = require('fs')
+const api = require('@cityofzion/neon-js').api
+
+const tokenDataUrl = 'http://notifications.neeeo.org/v1/tokens'
+const baseImageUrl = `https://raw.githubusercontent.com/${
+	process.env.PROJECT_NAME
+}/neo-tokens/master/assets`
+const net = 'MainNet'
+const NETWORK_ID = {
+	MAINNET: '1'
+}
+let promises
+const tokenData = {}
+
+const blackList = {
+	NRV: '2e25d2127e0240c6deaf35394702feb236d4d7fc'
+}
+
+api.loadBalance(api.getRPCEndpointFrom, { net }).then(endpoint => {
+	fetch(tokenDataUrl)
+		.then(data => data.json())
+		.then(response => {
+			promises = response.results.map(({ token }) => {
+				const { decimals, symbol, script_hash, name } = token
+				const scriptHash = script_hash.startsWith('0x')
+					? script_hash.slice(2)
+					: script_hash
+				return api.nep5.getToken(endpoint, scriptHash).then(tokenInfo => {
+					let image = ''
+					const formattedSymbol = symbol.toLowerCase()
+					if (fs.existsSync(`./assets/svg/${formattedSymbol}.svg`)) {
+						image = `${baseImageUrl}/svg/${formattedSymbol}.svg`
+					} else if (fs.existsSync(`./assets/png/${formattedSymbol}.png`)) {
+						image = `${baseImageUrl}/png/${formattedSymbol}.png`
+					}
+					if (!blackList[symbol] && blackList[symbol] !== scriptHash) {
+						tokenData[symbol] = {
+							symbol,
+							companyName: name,
+							type: 'NEP5',
+							networks: {
+								[NETWORK_ID.MAINNET]: {
+									name,
+									hash: scriptHash,
+									decimals,
+									totalSupply: tokenInfo && tokenInfo.totalSupply
+								}
+							},
+							image
+						}
+					}
+				})
+			})
+			Promise.all(promises).then(() => {
+				fs.writeFile('tokenList.json', JSON.stringify(tokenData, null, 2))
+			})
+		})
+})
