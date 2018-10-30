2f377f308624c111943ae36af0a0964cc2f819ff
Jeroen Peeters <contact@jeroenpeeters.me>
2018-10-30 12:11:10 +0100

    Use neeeo.org notifications server
    n1.cityofzion.io is currently showing incorrect token data, as the nOS token is not being displayed. It *is*, however, being displayed at the neeeo.org notification server.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fetch = require('isomorphic-fetch')
 const fs = require('fs')
 const { api } = require('@cityofzion/neon-js')
 
-const tokenDataUrl = 'https://n1.cityofzion.io/v1/tokens'
+const tokenDataUrl = 'http://notifications1.neeeo.org/v1/tokens'
 const baseImageUrl = `https://rawgit.com/${process.env.PROJECT_NAME}/neo-tokens/master/assets`
 
 const NETWORK_ID = {
